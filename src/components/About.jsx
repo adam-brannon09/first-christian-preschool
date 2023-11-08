dc50235e1ba72c3ed24ac5d6ffc98234a2b65616
adam-brannon09 <adam.brannon09@gmail.com>
2023-11-08 14:59:40 -0600

    fixed some sizing issues with images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,14 @@ import VerticalContact from "./VerticalContact"
 
 function About() {
     return (
-        <div className="flex flex-wrap w-full min-h-screen lg:flex-row my-9 place-content-center" id="about">
+        <div className="flex flex-wrap w-full min-h-screen lg:flex-row my-9 place-content-center " id="about">
 
-            <div className=" grid flex w-1/2 card place-items-center ml-8 ">
+            <div className=" grid flex w-1/2 card place-items-center ml-8 paragraph-mobile">
                 <h1 className="font-bold text-3xl justify-center">Welcome To First Christian Preschool</h1>
                 <p className="text-xl sm:text-lg md:text-xl lg:text-xl leading-relaxed"> At First Christian Preschool, we provide families with a nurturing, Christ-centered atmosphere where young children can flourish spiritually, intellectually, socially, emotionally, and physically. We believe every child is exceptional, brimming with boundless potential. Our commitment is to unlock these possibilities and support your child's journey to becoming the unique and special person that God intended them to be.
                 </p>
             </div>
-            <div className="divider divider-horizontal"></div>
+            {/* <div className="divider divider-horizontal"></div> */}
 
             <div className="mt-10">
                 <VerticalContact className="grid about-one" />
--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -4,10 +4,10 @@ import Building from "../assets/building.jpeg"
 function Mission() {
     return (
         <div className="hero min-h-screen border-shadow background text-white" id="philosophy">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={Building} className="max-w-md rounded-lg shadow-1xl" />
+            <div className="hero-content flex-col lg:flex-row-reverse paragraph-mobile">
+                <img src={Building} className=" shrink sm:max-w-sm lg:max-w-md rounded-lg shadow-1xl" />
                 <div>
-                    <h1 className="text-5xl font-bold">Our Philosophy</h1>
+                    <h1 className="text-5xl text-center font-bold">Our Philosophy</h1>
                     <p className="py-6 text-2xl">
                         At FCP, we believe in the development of the whole child socially, emotionally, academically and spiritually. Within the first couple weeks of school, the children are assessed and the curriculum is tailored to meet their needs. We accomplish this through what children do best: PLAY!
                     </p>
